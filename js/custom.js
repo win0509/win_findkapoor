ec5b8e13f6d25c91182d9a4add38bed15c89a3fc
win0509 <dbtmdfl1109@naver.com>
2023-07-15 21:00:16 +0900

    product section까지 완료

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,38 @@
-//AOS
-// AOS.init()
+
+// Header Show
+const Header = document.querySelector('header');
+
+window.addEventListener('scroll', function(){
+const scryTop = window.scrollY;
+
+if(scryTop > 0){
+    Header.classList.add('active');
+}else{
+    Header.classList.remove('active');
+}
+});
+
+
+//Swiper Main slide Navigation
+var swiper = new Swiper(".main-slides", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+//Swiper New Product section
+var swiper = new Swiper(".product-slides", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+
